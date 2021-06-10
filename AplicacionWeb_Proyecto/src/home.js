8d11d7f737d42f28ae51ca660f5de81ff52145a7
danielcotes2504 <danielenrique2000@hotmail.com>
2021-06-10 07:59:49 -0500

    Version final casi
    Tiene varios detalles de interfaz finalizados, solo falta el control de quemas

--- AplicacionWeb_Proyecto/src/home.js
+++ AplicacionWeb_Proyecto/src/home.js
@@ -2,4 +2,17 @@ window.onload = function() {
     verificacion()
 }
 const bienvenida = document.querySelector('#h1-welcome')
-bienvenida.innerHTML = `Bienvenido ${sessionStorage.nombre_usuario}`
+bienvenida.innerHTML = `¡Bienvenido ${sessionStorage.nombre_usuario}!`
+
+document.querySelector('#btn-usuario').addEventListener("click", (e) => {
+    window.location.href = "usuarios.html"
+
+})
+document.querySelector('#btn-dispositivo').addEventListener("click", (e) => {
+    window.location.href = "datos-nodo.html"
+
+})
+document.querySelector('#btn-horario').addEventListener("click", (e) => {
+    window.location.href = "horarios-programados.html"
+
+})
